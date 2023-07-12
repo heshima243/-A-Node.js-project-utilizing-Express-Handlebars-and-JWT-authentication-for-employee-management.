5973fa11d297d4250347f18e24684dcc341e9b9e
heshima243 <heshimajulien.official@gmail.com>
2023-07-12 21:21:08 +0200

    second save

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const con = require('./services/db');
 const { engine } = require('express-handlebars');
 const todoRoutes = require('./routes/todoRoutes');
 const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
 
 // Set the port number
-const PORT = 5000;
+const PORT = 6000;
 
 // Create an instance of the express application
 const app = express();
